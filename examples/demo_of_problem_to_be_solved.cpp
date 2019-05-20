41c6c728c5e6f2a3fcfc1109271534cddd69be74
Paul Dreik <github@pauldreik.se>
2019-05-20 19:58:20 +0200

    show problematic floating point behaviour

--- examples/demo_of_problem_to_be_solved.cpp
+++ examples/demo_of_problem_to_be_solved.cpp
@@ -17,8 +17,8 @@
 void
 example1_wrong_result()
 {
-  using mSec = std::chrono::duration<int, std::milli>;
   using Sec = std::chrono::duration<int>;
+  using mSec = std::chrono::duration<int, std::milli>;
 
   // converting a large number of seconds to milliseconds wont fit
   // in the target type, so this eventually ends up in implementation defined
@@ -31,11 +31,11 @@ example1_wrong_result()
 void
 example2_undefined_behaviour()
 {
-  using mSec = std::chrono::duration<long long, std::milli>;
   using Sec = std::chrono::duration<long long>;
+  using mSec = std::chrono::duration<long long, std::milli>;
 
   // this will cause an internal signed overflow inside chrono, which is
-  // undefined behaviour. Here is what ubsan says:
+  // undefined behaviour. Here is what ubsan says on my machine:
   // /usr/include/c++/7/chrono:176:38: runtime error: signed integer overflow:
   // 9223372036854776 * 1000 cannot be represented in type 'long long int'
 
@@ -44,9 +44,30 @@ example2_undefined_behaviour()
   assert(to.count() / 1000 == from.count() && "oops, wrong answer!");
 }
 
+void
+example3_wrong_result()
+{
+  using Sec = std::chrono::duration<double>;
+  using mSec = std::chrono::duration<int, std::milli>;
+
+  // converting a large number of seconds to milliseconds wont fit
+  // in the target type, so this eventually ends up in implementation defined
+  // behaviour and it gives the wrong result. prove this using a large
+  // tolerance.
+  using LargeInt = long long;
+  const LargeInt expected_ms = LargeInt{ INT_MAX } / 1000 * 1000;
+  const Sec from{ expected_ms / 1000 + 1 }; // -1 works fine, +1 overflows
+  const mSec to = std::chrono::duration_cast<mSec>(from);
+  const auto diff_ms = to.count() - expected_ms;
+  std::cout << " from=" << from.count() << "s to=" << to.count()
+            << "ms diff=" << diff_ms << "ms\n";
+  assert(std::abs(diff_ms) <= 1000 && "oops, wrong answer!");
+}
+
 int
 main(int argc, char* argv[])
 {
   //  example1_wrong_result();
-  example2_undefined_behaviour();
+  // example2_undefined_behaviour();
+  example3_wrong_result();
 }
